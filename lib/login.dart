ea329eb03db1d228d6fbd6fb2de2ec4371d41d1c
init_6 <iyaadluq@gmail.com>
2024-01-30 13:02:33 +0530

    Implemented a api login

--- lib/login.dart
+++ lib/login.dart
@@ -1,8 +1,40 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 class LoginScreen extends StatelessWidget {
+  final TextEditingController _regsiter_controller = TextEditingController();
+  final TextEditingController _dob_controller = TextEditingController();
+
+Future<String?> login(String username, String password) async {
+  final url = 'http://studentlogin.kcgcollege.ac.in';
+  final headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.71 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Referer': 'http://studentlogin.kcgcollege.ac.in/',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+  };
+  final body = '__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=&__VIEWSTATE=%2FwEPDwUKLTg2ODkwMDI5NQ9kFgICAw9kFgoCAw8QDxYGHg1EYXRhVGV4dEZpZWxkBQhjb2xsbmFtZR4ORGF0YVZhbHVlRmllbGQFDGNvbGxlZ2VfY29kZR4LXyFEYXRhQm91bmRnZBAVARlLQ0cgQ29sbGVnZSBvZiBUZWNobm9sb2d5FQECMTMUKwMBZxYBZmQCBQ8QZBAVAgtSb2xsIE51bWJlchFSZWdpc3RlcmVkIE51bWJlchUCATABMRQrAwJnZxYBAgFkAgcPD2QWBB4LcGxhY2Vob2xkZXIFEVJlZ2lzdGVyZWQgTnVtYmVyHgxhdXRvY29tcGxldGUFA29mZmQCCw8PZBYCHwQFA29mZmQCDw8PFgIeB1Zpc2libGVoZGRkVGTXEWkyJ%2FWMj4QNaVxx4miBw8LjRwBUqW%2FxWeqCSkc%3D&__VIEWSTATEGENERATOR=CA0B0334&__EVENTVALIDATION=%2FwEdAAeoMw5oUf6Bx7qoookBEHen1ewWtm3evXPJ0S9N%2F1pup%2FolUdBTEtKbUYVn9qLUVnP36l7NJf9XLe0xTP1byily7ATayzSAKKfWGUr2Dqcb%2BZxpWckI3qdmfEJVCu2f5cHN%2BDvxnwFeFeJ9MIBWR6934vHvarPjiaE44I2KpHcf1yDl%2FtX9t7eeyi4U5XVAg1o%3D&rblOnlineAppLoginMode=1&txtuname=$username&txtpassword=$password&Button1=Login';
+  final response = await http.post(Uri.parse(url), headers: headers, body: body);
+  
+  final sessionId = response.headers['set-cookie']?.split(';').firstWhere((s) => s.startsWith('ASP.NET_SessionId=')).split('=')[1];
+  
+  final redirectUrl = RegExp(r'href="([^"]+)"').firstMatch(response.body)?.group(1);
+  
+  final response2 = await http.get(Uri.parse(url + redirectUrl!), headers: {...headers, 'Cookie': 'ASP.NET_SessionId=$sessionId'});
+  
+  final name = RegExp(r'<span id="lblsname" class="ddl-lbl" style="">([^<]+)</span>').firstMatch(response2.body)?.group(1);
+  
+  debugPrint('NAMMEEE -----------------> >>> > > > > > >> > >  > > > $name');
+  return name;
+}
+
+
   @override
   Widget build(BuildContext context) {
     double hlen = MediaQuery.of(context).size.height;
@@ -58,6 +90,7 @@ class LoginScreen extends StatelessWidget {
                     child: Material(
                       color: Colors.transparent,
                       child: TextField(
+                        controller: _regsiter_controller,
                         decoration: InputDecoration(
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(22.0),
@@ -101,6 +134,7 @@ class LoginScreen extends StatelessWidget {
                     child: Material(
                     color: Colors.transparent,
                       child: TextField(
+                        controller: _dob_controller,
                         decoration: InputDecoration(
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(22.0),
@@ -127,7 +161,9 @@ class LoginScreen extends StatelessWidget {
               SizedBox(height: hlen * 0.07),
               Center(
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    login(_regsiter_controller.text, _dob_controller.text);
+                  },
                   child: Text('Login'),
                   style: ElevatedButton.styleFrom(
                     elevation: 10,
@@ -144,7 +180,7 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
   ),
-  
+
         
         );
   }
